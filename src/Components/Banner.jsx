81dc0482039aa8ed1331a71edb3fd1b71bab7cdd
siham-wedeh <siham.wedeh@gmail.com>
2025-05-19 22:40:01 +0300

    tree

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -47,7 +47,7 @@ function Banner() {
         <div className="flex-buttons">
           <div className="btn">
             <a href="/pdf/siham_cv.pdf" target="_blank" download>
-              <span>My Resume</span>
+              <span>My Resume</span> 
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="w-full h-auto !w-6 ml-1">
                 <path fill="none" d="M0 0h24v24H0z" />
                 <path fill="none" stroke="#222" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 7H6a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2v-5m-7 1L20 4m-5 0h5v5" />
