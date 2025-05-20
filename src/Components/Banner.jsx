b3cf07e10db78a8d2df34e11e9301bf43948ca4c
siham-wedeh <siham.wedeh@gmail.com>
2025-05-20 12:56:07 +0300

    www

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -37,7 +37,7 @@ function Banner() {
       </div>
       <div className="text-dec " data-aos="fade-left">
         <div> 
-          Hi all, I'm
+          Hi all, I'meeeeeeeeeeeeeee
           <br />
           <span ref={autoTextRef} className="color-title"></span>
         </div>
